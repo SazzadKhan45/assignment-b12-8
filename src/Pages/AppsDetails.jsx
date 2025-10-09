584b1cfdfff96086f6997fc296c8bf52775a9f26
Sazzadkhan45 <sazzadkhan753@gmail.com>
2025-10-09 15:52:41 +0600

    Rechart successfuly design

--- src/Pages/AppsDetails.jsx
+++ src/Pages/AppsDetails.jsx
@@ -5,8 +5,12 @@ import ReviewsImg from "../assets/icon-review.png"
 import downloadImg from "../assets/icon-downloads.png"
 import starImg from "../assets/icon-ratings.png"
 import { toast } from "react-toastify";
+import { useState } from "react";
+import { BarChart, Bar, XAxis, YAxis, Tooltip, ResponsiveContainer, CartesianGrid, } from "recharts";
 
 const AppsDetails = () => {
+
+    const [install, setInstall] = useState(false);
     const { id } = useParams();
     const { appsData } = useHeroApps();
 
@@ -17,38 +21,42 @@ const AppsDetails = () => {
             <div className="bg-[#f1f1f1] py-8">
                 <Container>
                     <p className="text-center text-red-500 font-semibold">
-                        App not found or still loading...
+                        <span className="loading loading-bars loading-xl"></span>
                     </p>
                 </Container>
             </div>
         );
     }
 
-    const { image, title, description, downloads, ratingAvg, reviews, size } = singleApp;
+    const { image, title, description, downloads, ratingAvg, reviews, size, ratings } = singleApp;
+
+    const reChartInfo = [...ratings].sort((a, b) => parseInt(b.name) - parseInt(a.name));
 
     // console.log(singleApp)
 
     const handleAppsAddLocalStorage = () => {
 
         const appsListToAdd = JSON.parse(localStorage.getItem('appsList'));
-        let updateAppsList =[];
+        let updateAppsList = [];
 
-        if(appsListToAdd){
+        if (appsListToAdd) {
 
-            const isDuplicate =appsListToAdd.some(app => app.id === singleApp.id);
-            
-            if(isDuplicate){
-                toast('This is Apps Already Installed')
+            const isDuplicate = appsListToAdd.some(app => app.id === singleApp.id);
+
+            if (isDuplicate) {
+                toast('Apps Already Installed')
                 return;
             }
-            updateAppsList= [...appsListToAdd, singleApp];
+            updateAppsList = [...appsListToAdd, singleApp];
         }
-        else{
+        else {
             updateAppsList.push(singleApp);
         }
+        toast('Apps Installing...')
 
         localStorage.setItem('appsList', JSON.stringify(updateAppsList));
-    }
+        setInstall(true);
+    };
 
     return (
         <div className="bg-[#f1f1f1] py-8 px-2 md:px-0">
@@ -82,8 +90,37 @@ const AppsDetails = () => {
                                 </div>
                             </div>
                             {/* Apps add to localstorage btn */}
-                            <button onClick={handleAppsAddLocalStorage} className="bg-[#00d390] text-white font-medium px-4 md:px-8 py-1.5 rounded-lg cursor-pointer mt-6">Install Now ({size}) MB</button>
+                            <button onClick={() => handleAppsAddLocalStorage()} className="bg-[#00d390] text-white font-medium px-4 md:px-8 py-1.5 rounded-lg cursor-pointer mt-6">
+                                {
+                                    install ? 'Install' : `Install Now (${size}) MB`
+                                }
+                            </button>
 
+                        </div>
+                    </div>
+
+                    {/* Rechart design */}
+                    <div className="py-6">
+                        <div className="w-full h-64 bg-gray-50 p-4 rounded-xl shadow pb-8">
+                            <h2 className="font-semibold mb-4">Ratings</h2>
+                            <ResponsiveContainer width="100%" height="100%">
+                                <BarChart
+                                    data={reChartInfo}
+                                    layout="vertical"
+                                    margin={{ top: 10, right: 20, left: 20, bottom: 10 }}
+                                >
+                                    <CartesianGrid horizontal={false} strokeDasharray="3 3" />
+                                    <XAxis type="number" />
+                                    <YAxis
+                                        dataKey="name"
+                                        type="category"
+                                        tick={{ fontSize: 12 }}
+                                        width={70}
+                                    />
+                                    <Tooltip />
+                                    <Bar dataKey="count" fill="#ff9100" radius={[0, 6, 6, 0]} barSize={20} />
+                                </BarChart>
+                            </ResponsiveContainer>
                         </div>
                     </div>
                     <div className="mt-6">
